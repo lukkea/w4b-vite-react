4c45c11804da0602497b5b4aa15cdc3e95076e69
Colin Hughes <colin@w4b.co.uk>
2023-03-22 14:34:17 +0000

    corrected errors in setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'lib',
-      fileName: (format) => `lib.${format}.ts`,
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'w4b-vite-react',
+      fileName: 'w4b-vite-react',
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
+          react: 'React',
+          'react-dom': 'reactDom',
           'react/jsx-runtime': 'jsxRuntime',
         },
       },
